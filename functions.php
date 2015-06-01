9126d17d377f5b785854ba57c2e637750f27583a
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2015-06-01 08:57:33 +0300

    remove imoji scripts and styles

--- functions.php
+++ functions.php
@@ -23,6 +23,8 @@ function my_remove_generators() {
   remove_action('wp_head', 'wp_generator');
   remove_action('wp_head', 'wlwmanifest_link');
   remove_action('wp_head', 'rsd_link');
+  remove_action( 'wp_head', 'print_emoji_detection_script', 7);
+  remove_action( 'wp_print_styles', 'print_emoji_styles' );
   if ( ! empty ( $GLOBALS['sitepress'] ) ) {
     remove_action( 'wp_head', array( $GLOBALS['sitepress'], 'meta_generator_tag' ) );
   }
